5febbd240db290e6b01e5acec4a44aebc5902551
kingawi <kingawisniewska1996@gmail.com>
2023-02-01 14:08:49 +0100

    get exercises

--- app/routes/exercise.routes.js
+++ app/routes/exercise.routes.js
@@ -1,68 +1,57 @@
 const express = require('express')
+const db = require('../model')
 // const router = express.Router();
-const homeController = require('../controllers/home.controller')
-const uploadController = require('../controllers/exercise.controller')
 
-// let exerciseUpload = app => {
-//   router.get("/", homeController.getHome);
-
-//   router.post("/upload", uploadController.uploadFiles);
-//   router.get("/files", uploadController.getListFiles);
-//   router.get("/files/:name", uploadController.download);
-
-//   return app.use("/", router);
-// };
-//
-// module.exports = exerciseUpload;
-
-// module.exports = function (app) {
-//     app.use(function (req, res, next) {
-// 		res.header('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept')
-// 		next()
-// 	})
-//     app.get("/", homeController.getHome);
-
-//     app.post("/upload", uploadController.uploadFiles);
-//     app.get("/files", uploadController.getListFiles);
-//     app.get("/files/:name", uploadController.download);
-
-// }
+const ROLES = db.ROLES
+const User = db.user
+const Exercises = db.exercise
 
 //post
 //pobierz wszystkie - get
 //update rekordu
 //delete
-
-app.get('/trainerExercises', (req, res) => {
-	const trainerExercises = [] //
-	if (exercise.user._id == req.body.user._id) {
-		let exe = db.exercises.find({ exerciseCreator: req.body.user._id })
-		forEach({ exe })
-		{
-			trainerExercises.push(exercises.user._id)
+//!!!!! dodac module export
+module.exports = function (app) {
+	app.use(function (req, res, next) {
+		res.header('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept')
+		next()
+	})
+	app.get('/trainerExercises', (req, res) => {
+		const trainerExercises = [] //
+		if (db.exerciseCreator === req.body._id) {
+			trainerExercises.push(
+				Exercises.find({ exerciseCreator: req.body._id }).select({
+					exerciseName: 1,
+					exerciseDescription: 1,
+					exerciseAddingDate: 1,
+					exerciseCreator: 1,
+					_id: 1,
+				})
+			)
 			console.log(trainerExercises)
-		}}
-    else {
-            res.status(404).send({ message: "Nie znaleziono ćwiczeń dodanych przez trenera" });
-            return;
-        }
-	}
-)
-app.get("/trainerExercise", async (req, res) => {
-    try {
-      const trainerExercise = await User.find().populate({
-        path: "exercises",
-        select: "exerciseName exerciseDescription"
-      })
-  
-      res.status(200).json({ data: exercises });
-    } catch (error) {
-      res.status(500).json({ error });
-    }
-  });
-	//-jak wrzucic wyswietlone wyniki do tablicy trainerExercises i pozniej to wyswietlic?
-	//zrobic petle for i in db.xercise krotsze od exe.length dodaj do tablicy i cgl tablice
-	//exec() method executes a search for a match in a specified string and returns a result array, or null
+			res.end()
+		} else {
+			res.status(404).send({ message: 'Nie znaleziono ćwiczeń dodanych przez trenera' })
+		}
+	})
+}
+// app.get('/trainerExercise', async (req, res) => {
+// 	try {
+// 		const trainerExercise = await User.find().populate({
+// 			path: 'exercises',
+// 			select: 'exerciseName exerciseDescription',
+// 		})
+
+// 		res.status(200).json({ data: exercises })
+// 		console.log(trainerExercise, 'dziala bez s')
+// 	} catch (error) {
+// 		res.status(500).json({ error })
+// 	}
+// })
+
+//-jak wrzucic wyswietlone wyniki do tablicy trainerExercises i pozniej to wyswietlic?
+//zrobic petle for i in db.xercise krotsze od exe.length dodaj do tablicy i cgl tablice
+//exec() method executes a search for a match in a specified string and returns a result array, or null
 
 // 	const Exercise = db.exercise
 // 	Exercise.find({
